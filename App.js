ae89378ee1324da5b355a500002ac50d18b8f094
rexarabe <ab28041993@gmail.com>
2021-06-13 11:36:13 +0300

    I modified some lines. To fix the bug for iterables.

--- App.js
+++ App.js
@@ -1,36 +1,39 @@
 const item = document.querySelector('.item')
-const placeholders = document.querySelector('.placeholder')
+const placeholders = document.querySelectorAll('.placeholder')
 
 item.addEventListener('dragstart', dragstart)
 item.addEventListener('dragend', dragend)
 
-for (const placeholder of placeholders){
+for (const placeholder of placeholders) {
     placeholder.addEventListener('dragover', dragover)
     placeholder.addEventListener('dragenter', dragenter)
     placeholder.addEventListener('dragleave', dragleave)
     placeholder.addEventListener('drop', dragdrop)
 }
 
-function dragstart(event){
+function dragstart (event) {
     event.target.classList.add('hold')
-    setTimeout(() =>     event.target.classList.add('hide'), 0
-    )
+    setTimeout(() => event.target.classList.add('hide'), 0)
 }
-function dragend(event){
+
+function dragend (event) {
     event.target.className = 'item'
-
 }
-function dragover(event){
-    console.log()
 
+function dragover (event) {
+    event.preventDefault()
 }
-function dragenter(event){
 
-}
-function dragleave(event){
+function dragenter (event) {
+    event.target.classList.add('hovered')
 
 }
 
-function dragdrop(event){
+function dragleave (event) {
+    event.target.classList.remove('hovered')
+}
 
+function dragdrop (event) {
+    event.target.classList.remove('hovered')
+    event.target.append(item)
 }
